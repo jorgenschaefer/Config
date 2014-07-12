a4e9d721dd1cd5324fb09f9ab108ca3b5a1fa598
Jorgen Schaefer <contact@jorgenschaefer.de>
2014-07-12 16:10:08 +0200

    Automatic commit

--- virtualenvs_postactivate.sh
+++ virtualenvs_postactivate.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # This hook is run after every virtualenv is activated.
-if [ -f "$VIRTUAL_ENV/.project" ] && [ -f "$(cat "$VIRTUAL_ENV/.project")/setup.sh" ]
+if [ -f "$VIRTUAL_ENV/.project" ]
 then
-    . "$(cat "$VIRTUAL_ENV/.project")/setup.sh"
+    PROJECT_ROOT="$(cat "$VIRTUAL_ENV/.project")"
+    for filename in setup.sh .venv-setup.sh
+    do
+        if [ -f "$PROJECT_ROOT/$filename" ]
+        then
+            . "$PROJECT_ROOT/$filename"
+        fi
+    done
+    if [ -f "$PROJECT_ROOT/.env" ]
+    then
+        export $(cat "$PROJECT_ROOT/.env" | grep '^[^#]')
+    fi
 fi
