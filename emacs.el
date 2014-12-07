e75697a5683fe55df1e20b0885f75b651c3b39e5
Jorgen Schaefer <contact@jorgenschaefer.de>
2014-12-07 13:10:08 +0100

    Automatic commit

--- emacs.el
+++ emacs.el
@@ -732,6 +732,9 @@ glyph."
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/")
              t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/")
+             t)
 (package-initialize)
 
 ;;;;;;;;;
