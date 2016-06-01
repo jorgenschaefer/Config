9808f30ad116ca7335640b5b370fba8d5cef374e
Jorgen Schaefer <Jorgen.Schaefer@mindmatters.de>
2016-06-01 09:10:08 +0200

    Automatic commit

--- bash.sh
+++ bash.sh
@@ -82,7 +82,8 @@ fi
 
 if [ -d "$HOME/.rbenv" ]
 then
-    export PATH="$HOME/.rbenv:$PATH"
+    export PATH="$HOME/.rbenv/bin:$PATH"
+    eval "$(rbenv init -)"
 fi
 
 if [ -d "/usr/lib/chromium" ]
